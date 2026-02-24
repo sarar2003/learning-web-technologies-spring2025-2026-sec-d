9ec670e1f65f3bf052aceb087e27bbff8652adc1
sarar2003 <saifurtaher40@gmail.com>
2026-02-24 15:33:04 +0600

    updated name.js

--- Lab 2/name.js
+++ Lab 2/name.js
@@ -1,13 +1,22 @@
-// let name = document.getElementById("name");
-let value = document.getElementById("name").value;
-
 let btn = document.getElementById("submit-btn");
 btn.addEventListener("click", verify);
 
-function verify(value){
-    value = value.trim();
+function verify(event){
+    event.preventDefault();
+    let value = document.getElementById("name").value.trim();
     if(value === ""){
-        console.log("Please enter your name");
-        alert("Please enter your name");
+        console.log("can not be empty");
+    }
+    else if(!/^[A-Zz-z]/.test(value) ){
+        console.log("Must start with a letter");
+    }
+    else if(!/^[A-Zz.\-\s]+$/.test(value)){
+        console.log("only letters, dot(.), dash(-) allowed");
+    }
+    else if(value.split(" ").length < 2){
+        console.log("Must contain at least 2 words");
+    }
+    else {
+        console.log("Input Valid");
     }
 }
